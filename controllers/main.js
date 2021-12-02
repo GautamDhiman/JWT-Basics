803d58748abb6021bdf572a56c22abbfa4a4992c
GautamDhiman <gautamCodesHere@gmail.com>
2021-12-02 14:09:55 +0530

    refractored code, shifted auth to middleware

--- controllers/main.js
+++ controllers/main.js
@@ -22,26 +22,8 @@ const login = async(req, res) => {
 
 const dashboard = async (req, res) => {
 
-    const authHeader = req.headers.authorization;
-
-    if(!authHeader || !authHeader.startsWith('Bearer '))
-    {
-        throw new CustomAPIError('No Token Provided', 401);
-    }
-
-    const token = authHeader.split(' ')[1];
-    
-    try {
-        const decoded = jwt.verify(token, process.env.JWT_SECRET)
-
-        const luckyNumber = Math.floor(Math.random()*100)
-        res.status(200).json({ msg: `Hello ${decoded.username}`, secret: `here is your authorized data, ${luckyNumber}`});
-
-    } catch (error) {
-        throw new CustomAPIError('Not Authorized to access this route', 401);
-    }
-
-    
+    const luckyNumber = Math.floor(Math.random()*100)
+    res.status(200).json({ msg: `Hello ${req.user.username}`, secret: `here is your authorized data, ${luckyNumber}`});
 }
 
 module.exports = { login, dashboard }
